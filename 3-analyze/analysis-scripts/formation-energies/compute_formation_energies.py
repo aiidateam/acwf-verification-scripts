f70041fd8df10ae2ebb4556c0d22b8b0503291af
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2022-03-17 15:56:17 +0100

    Modifying scripts for formation energy
    Now they can also compute the minimum-energy configuration of unaries
    They still need some assessment of correctness
    
    I checked and, with the current data (probably not very good?)
    I get for
    Pb-X|X2O|O
    a large error of 6.878987 for the formation energy
    (the two values being: -7.728713295810546 vs -0.8497267791535705)
    
    Indeed, if I look at the values I see:
    Pb-X2O (3 atoms/simulation)
      FLEUR: "E0": -1141042.0321028265
      WIEN : "E0": -1141021.3817287854
    
    Lowest-energy Pb unary -> FCC
    Pb-X/FCC (1 atom/simulation)
     FLEUR "E0": -569487.4147870957
     WIEN  "E0": -569487.4066672832
    
    Lowest-energy O unary -> SC
    O-X/SC (1 atom/simulation)
     WIEN  "E0": -2044.019213881717
     FLEUR "E0": -2044.016388747669
    
    So the unaries agree quite well, but there are 21eV (!!)
    of difference between the oxide calculation of FLEUR and WIEN2K.
    Divided by 3 atoms, it is indeed ~7eV/atom, so the script seems correect.
    
    Interestingly, if one computes the formation energies using *only*
    oxides and endpoints, the results are not so bad.
    So probably one of the two codes has a "consistent" shift for all oxides,
    that is not applied for the unaries.
    
    It would be interesting to see if this is still the case with the most recent data.

--- 3-analyze/analysis-scripts/formation-energies/compute_formation_energies.py
+++ 3-analyze/analysis-scripts/formation-energies/compute_formation_energies.py
@@ -7,51 +7,87 @@ import sys
 import ase.data
 import numpy as np
 
-# TODO: add back oxygen
-ALL_ELEMENTS = [ase.data.chemical_symbols[Z] for Z in range(1, 96+1) if Z != 8]
+UNARIES_CONFIGURATIONS = ['X/BCC', 'X/SC', 'X/FCC', 'X/Diamond']
+OXIDES_CONFIGURATIONS = ['XO', 'XO2', 'XO3', 'X2O', 'X2O3', 'X2O5']
+ALL_ELEMENTS = [ase.data.chemical_symbols[Z] for Z in range(1, 96+1)]
 
-def get_num_atoms(configuration):
-    conf_natoms_map = {'XO': 2, 'XO2': 3, 'XO3': 4, 'X2O': 3, 'X2O3': 10, 'X2O5': 14}
-    return conf_natoms_map[configuration]
-
-def get_ratio_to_formula_unit(configuration):
-    conf_map = {'XO': 1, 'XO2': 1, 'XO3': 1, 'X2O': 1, 'X2O3': 2, 'X2O5': 2}
-    return conf_map[configuration]
 
 def get_num_atoms_in_formula_unit(configuration):
-    num_atoms_in_cell = get_num_atoms(configuration)
-    ratio = get_ratio_to_formula_unit(configuration)
-    assert num_atoms_in_cell % ratio == 0
-    return num_atoms_in_cell // ratio
+    return sum(get_X_O_in_formula_unit(configuration))
 
 def get_X_O_in_formula_unit(configuration):
-    map = {'XO': (1, 1), 'XO2': (1, 2), 'XO3': (1, 3), 'X2O': (2, 1), 'X2O3': (2, 3), 'X2O5': (2, 5)}
-    return map[configuration] 
+    map = {
+        'XO': (1, 1), 'XO2': (1, 2), 'XO3': (1, 3),
+        'X2O': (2, 1), 'X2O3': (2, 3), 'X2O5': (2, 5),
+        # For the unaries I could put any value, even (2, 0) or similar; anyway in the end
+        # everything is renormalized per atom. It is however important that they are all the same,
+        # to properly deal with the cases below ('X' meaning lowest-in-energy X/..., and O meaning
+        # lowest-in-energy X/... for element 'X=oxygen')
+        'X/BCC': (1, 0), 'X/SC': (1, 0), 'X/FCC': (1, 0), 'X/Diamond': (1, 0),
+        # Deal with the special cases
+        'X': (1, 0), 'O': (0, 1)
+        }
+    return map[configuration]
 
 def get_O_percentage(configuration):
     X, O = get_X_O_in_formula_unit(configuration)
     return O / (X + O)
 
-def get_formation_energy(element, configuration_triple, plugin):
+def _get_energy_internal(plugin, element, configuration):
+    system = f'{element}-{configuration}'
+    return plugin['BM_fit_data'][system]['E0'] / plugin['num_atoms_in_sim_cell'][system] * get_num_atoms_in_formula_unit(configuration)
+
+
+def get_energy_per_formula_unit(plugin_unaries, plugin_oxides, element, configuration, unaries_energy_diff=None):
+    if configuration in UNARIES_CONFIGURATIONS + ['X', 'O']:
+        plugin = plugin_unaries
+    elif configuration in OXIDES_CONFIGURATIONS:
+        plugin = plugin_oxides
+    else:
+        raise ValueError("Unknown configuration string")
+    
+    if configuration == 'X':
+        if unaries_energy_diff is None:
+            raise ValueError("You need to pass `unaries_energy_diff` if one of the configurations is 'X'")
+        # Enforce comparing with the energy of the reference element
+        return _get_energy_internal(plugin, element, unaries_energy_diff[element]['reference_configuration'])
+    elif configuration == 'O':
+        if unaries_energy_diff is None:
+            raise ValueError("You need to pass `unaries_energy_diff` if one of the configurations is 'X'")
+        if unaries_energy_diff is None:
+            raise ValueError("You need to pass `unaries_energy_diff` if one of the configurations is 'X'")
+        # Enforce comparing with the energy of oxygen, independent of the element
+        return _get_energy_internal(plugin, 'O', unaries_energy_diff['O']['reference_configuration'])
+
+    else:
+        return _get_energy_internal(plugin, element, configuration)
+
+
+def get_formation_energy(element, configuration_triple, plugin_unaries, plugin_oxides, unaries_energy_diff):
     """
-    Given an element and three configurations to compare, compute the formation energy 
+    Given an element and three configurations to compare, compute the formation energy per atom
     of the central configuration w.r.t. to the two extremal ones.
 
-    You need to pass the plugin data, from which E0 is obtained.
+    You need to pass the two JSON-loaded data from the plugin (for unaries and for oxides),
+    from which E0 is extracted.
     """    
     assert len(configuration_triple) == 3
 
+    assert plugin_unaries['script_version'] == '0.0.3'
+    assert plugin_oxides['script_version'] == '0.0.3'
+
     O_percentages = [get_O_percentage(conf) for conf in configuration_triple]
-    assert O_percentages[0] < O_percentages[2], "percentage_left must be < percentage_right"
+    assert O_percentages[0] < O_percentages[2], "percentage_left must be <= percentage_right"
     assert O_percentages[1] >= O_percentages[0], "percentage must be >= percentage_left"
     assert O_percentages[1] <= O_percentages[2], "percentage must be <= percentage_right"
 
-    try:
-        energies_per_formula_unit = [
-            plugin['BM_fit_data'][f'{element}-{configuration}']['E0'] / get_ratio_to_formula_unit(configuration) for configuration in configuration_triple
-        ]
-    except (KeyError, TypeError): # Something is None because of a fit problem, of wasn't even computed
-        return None
+    #try:
+    energies_per_formula_unit = [
+        get_energy_per_formula_unit(plugin_unaries, plugin_oxides, element, configuration, unaries_energy_diff)
+        for configuration in configuration_triple
+    ]
+    #except (KeyError, TypeError): # Something is None because of a fit problem, of wasn't even computed
+    #    return None
 
     X_L, O_L = get_X_O_in_formula_unit(configuration_triple[0])
     X_C, O_C = get_X_O_in_formula_unit(configuration_triple[1])
@@ -79,34 +115,91 @@ def get_formation_energy(element, configuration_triple, plugin):
     formation_per_formula_unit = energies_per_formula_unit[1] - reference_energy
     return formation_per_formula_unit / get_num_atoms_in_formula_unit(configuration_triple[1])
 
-def generate_json_data(plugin_name_1, plugin_name_2):
+def get_unaries_energy_difference(
+    plugin_name_1, plugin_name_2, plugin1_unaries, plugin2_unaries):
+    unaries_data = {}
+    for element in ALL_ELEMENTS:
+        plugin1_energies = []
+        plugin2_energies = []
+        # TODO: fix when there are missing elements
+        for configuration in UNARIES_CONFIGURATIONS:
+            # I pass None for the plugin_oxides since they should not be used
+            plugin1_energies.append(
+                get_energy_per_formula_unit(plugin1_unaries, None, element, configuration)
+            )
+            plugin2_energies.append(
+                get_energy_per_formula_unit(plugin2_unaries, None, element, configuration)
+            )
+        plugin1_min_energy_conf = UNARIES_CONFIGURATIONS[np.argmin(plugin1_energies)]
+        plugin2_min_energy_conf = UNARIES_CONFIGURATIONS[np.argmin(plugin2_energies)]
+        # I use the first plugin as reference
+        ref_min_energy_conf = plugin1_min_energy_conf
+
+        plugin1_energy_dict = dict(zip(UNARIES_CONFIGURATIONS, plugin1_energies))
+        plugin2_energy_dict = dict(zip(UNARIES_CONFIGURATIONS, plugin2_energies))
+        print(f"{element:2s}: {ref_min_energy_conf}")      
+        if plugin1_min_energy_conf != plugin2_min_energy_conf:
+            print(f"  >> WARNING! Different minimum-energy configuration for {element}! {plugin1_min_energy_conf} (plugin1) vs {plugin2_min_energy_conf} (plugin 2)")
+
+        unaries_data[element] = {'configurations': {}, 'reference_configuration': ref_min_energy_conf}
+        for configuration in UNARIES_CONFIGURATIONS:
+            unaries_data[element]['configurations'][configuration] = {}
+
+        for plugin_energy_dict, plugin_name in [
+            (plugin1_energy_dict, plugin_name_1),
+            (plugin2_energy_dict, plugin_name_2),
+        ]:
+            print(f"  - {plugin_name:10s} [eV/atom]:", end="")
+            print("    ", end="")
+            for configuration in UNARIES_CONFIGURATIONS:
+                en_diff = plugin_energy_dict[configuration] - plugin_energy_dict[ref_min_energy_conf]
+                unaries_data[element]['configurations'][configuration][plugin_name] = en_diff
+                if configuration != ref_min_energy_conf:
+                    print(f"({configuration:9s}: {en_diff:+4.3f})  ", end="")
+            print() # Newline
+
+    return unaries_data
+
+def generate_json_data(plugin_name_1, plugin_name_2, unaries_set="unaries-set2", oxides_set="set2plusoxygen"):
 
     try:
-        with open(f'../results-{plugin_name_1}.json') as fhandle:
-            plugin1 = json.load(fhandle)
-        with open(f'../results-{plugin_name_2}.json') as fhandle:
-            plugin2 = json.load(fhandle)
+        with open(f'../temp-results/results-{unaries_set}-{plugin_name_1}.json') as fhandle:
+            plugin1_unaries = json.load(fhandle)
+        with open(f'../temp-results/results-{unaries_set}-{plugin_name_2}.json') as fhandle:
+            plugin2_unaries = json.load(fhandle)
+        with open(f'../temp-results/results-{oxides_set}-{plugin_name_1}.json') as fhandle:
+            plugin1_oxides = json.load(fhandle)
+        with open(f'../temp-results/results-{oxides_set}-{plugin_name_2}.json') as fhandle:
+            plugin2_oxides = json.load(fhandle)
     except OSError as exc:
         print(f"Error, probably you didn't generate the file yet! Original error: {exc}")
         sys.exit(1)
 
-    all_data = {}
-
+    all_data = {
+        'formation_energies': {},
+        'unaries_energy_difference': get_unaries_energy_difference(
+            plugin_name_1, plugin_name_2, plugin1_unaries, plugin2_unaries)
+        }
     for element in ALL_ELEMENTS:
         for configuration_triple in [
                 ('X2O', 'XO', 'XO3'),
                 ('X2O', 'XO2', 'XO3'),
                 ('X2O', 'X2O3', 'XO3'),
                 ('X2O', 'X2O5', 'XO3'),
+                # 'X' is a placeholder for the lowest-energy 'X/...' phase, and 'O' for the
+                # lowest-energy 'X/...' phase with X=oxygen
+                #('X', 'X2O', 'O'),
+                #('X', 'XO', 'O'),
+                #('X', 'XO2', 'O'),
+                #('X', 'X2O3', 'O'),
+                #('X', 'X2O5', 'O'),
+                #('X', 'XO3', 'O'),
             ]:
-            #percentage_left = get_O_percentage(configuration_triple[0])
-            #percentage = get_O_percentage(configuration_triple[1])
-            #percentage_right = get_O_percentage(configuration_triple[2])
 
-            plugin1_formation_energy = get_formation_energy(element, configuration_triple, plugin1)
-            plugin2_formation_energy = get_formation_energy(element, configuration_triple, plugin2)
+            plugin1_formation_energy = get_formation_energy(element, configuration_triple, plugin1_unaries, plugin1_oxides, all_data['unaries_energy_difference'])
+            plugin2_formation_energy = get_formation_energy(element, configuration_triple, plugin2_unaries, plugin2_oxides, all_data['unaries_energy_difference'])
 
-            all_data[f"{element}-{configuration_triple[0]}/{configuration_triple[1]}/{configuration_triple[2]}"] = {
+            all_data['formation_energies'][f"{element}-{configuration_triple[0]}|{configuration_triple[1]}|{configuration_triple[2]}"] = {
                 plugin_name_1: plugin1_formation_energy,
                 plugin_name_2: plugin2_formation_energy
             }
@@ -117,14 +210,14 @@ def generate_json_data(plugin_name_1, plugin_name_2):
 
     fname = f'{OUT_FOLDER}/formation-energies-{plugin_name_1}-VS-{plugin_name_2}.json'
     with open(fname, 'w') as fhandle:
-        json.dump({'formation_energies': all_data}, fhandle, sort_keys=True, indent=2)
+        json.dump(all_data, fhandle, sort_keys=True, indent=2)
     print(f"File '{fname}' written.")
 
 if __name__ == "__main__":
     try:
-        plugin1 = sys.argv[1]
-        plugin2 = sys.argv[2]
+        plugin1_name = sys.argv[1]
+        plugin2_name = sys.argv[2]
     except IndexError:
         print("Pass as two parameters the two plugins to compare")
         sys.exit(1)
-    generate_json_data(plugin1, plugin2)
+    generate_json_data(plugin1_name, plugin2_name)
--- 3-analyze/analysis-scripts/formation-energies/plot_histo_formation_energies.py
+++ 3-analyze/analysis-scripts/formation-energies/plot_histo_formation_energies.py
@@ -9,7 +9,7 @@ from scipy.optimize import curve_fit
 
 
 BINS = 100
-FORM_ENERGY_PRINT_THRESHOLD = 0.03 # eV/atom
+PRINT_THRESHOLD = 0.03 # eV/atom
 
 OUT_FOLDER = 'formation-energies-output'
 
@@ -17,7 +17,59 @@ def gaussian(x, a, x0, sigma):
     return a * np.exp(-(x - x0)**2 / (2 * sigma**2))
 
 
-def generate_plots(plugin1, plugin2, x_zoom_factor=1., abs_x_range=None):
+def get_dissimilarities(plugin1, plugin2, what):
+
+    fname = f'formation-energies-{plugin1}-VS-{plugin2}.json'
+    try:
+        with open(f'{OUT_FOLDER}/{fname}') as fhandle:
+            raw_data = json.load(fhandle)
+    except OSError:
+        print(f"No file '{fname}' found in the '{OUT_FOLDER}' subfolder! Run ./compute_formation_energies.py first.")
+        sys.exit(1)
+
+    dissimilarities = []
+    missing_count = 0
+
+    if what == 'formation-energy':
+        formation_data = raw_data['formation_energies']
+        for system, data in formation_data.items():
+            # System is something like "Ac-X2O|X2O3|XO3"
+            plugin1_formation_energy = data[plugin1]
+            plugin2_formation_energy = data[plugin2]
+            if plugin1_formation_energy is None or plugin2_formation_energy is None:
+                # Deal with missing data for at least one plugin
+                missing_count += 1                
+                continue
+            dissimilarities.append((plugin2_formation_energy - plugin1_formation_energy, system, plugin1_formation_energy, plugin2_formation_energy))
+    elif what == 'unaries':
+        unaries_data = raw_data['unaries_energy_difference']
+        for element, data in unaries_data.items():
+            reference_configuration = data['reference_configuration']
+            for configuration, plugin_data in data['configurations'].items():
+                if configuration == reference_configuration:
+                    # Avoid to store zeros for the reference configuration, that would
+                    # bias the final plot
+                    continue
+                plugin1_energy_difference = plugin_data[plugin1]
+                plugin2_energy_difference = plugin_data[plugin2]
+                if plugin1_energy_difference is None or plugin2_energy_difference is None:
+                    # Deal with missing data for at least one plugin
+                    missing_count += 1
+                    continue
+                dissimilarities.append(
+                    (plugin2_energy_difference - plugin1_energy_difference,
+                    f"{element}/{configuration}-wrt-{reference_configuration}",
+                    plugin1_energy_difference,
+                    plugin2_energy_difference))
+    else:
+        raise ValueError(f"Unknown value of 'what': '{what}'")
+
+    if missing_count:
+        print(f"WARNING: {missing_count} systems missing when checking data for '{what}'")
+
+    return dissimilarities
+
+def generate_plots(plugin1, plugin2, what, x_zoom_factor=1., abs_x_range=None):
 
     # x_zoom_factor: Adapt this factor to change the zoom on the x axis
     # The default zoom is obtained from the standard deviation of the data
@@ -26,24 +78,6 @@ def generate_plots(plugin1, plugin2, x_zoom_factor=1., abs_x_range=None):
     # if abs_x_range is passed, x_zoom_factor is ignored and data is plotted in the range
     # [-abs_x_range, +abs_x_range]
 
-    fname = f'formation-energies-{plugin1}-VS-{plugin2}.json'
-    
-    try:
-        with open(f'{OUT_FOLDER}/{fname}') as fhandle:
-            formation_data = json.load(fhandle)['formation_energies']
-    except OSError:
-        print(f"No file '{fname}' found in the '{OUT_FOLDER}' subfolder! Run ./compute_formation_energies.py first.")
-        sys.exit(1)
-
-    formation_energy_dissimilarities = []
-    for system, data in formation_data.items():
-        # System is something like "Ac-X2O/X2O3/XO3"
-        plugin1_formation_energy = data[plugin1]
-        plugin2_formation_energy = data[plugin2]
-        if plugin1_formation_energy is None or plugin2_formation_energy is None:
-            # Deal with missing data for at least one plugin
-            continue
-        formation_energy_dissimilarities.append((plugin2_formation_energy - plugin1_formation_energy, system, plugin1_formation_energy, plugin2_formation_energy))
 
     # Plotting
     fig = pl.figure(figsize=(18,6))
@@ -61,7 +95,9 @@ def generate_plots(plugin1, plugin2, x_zoom_factor=1., abs_x_range=None):
     pl.rc('legend', fontsize=TINY_SIZE)    # legend fontsize
     pl.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-    flat_data = np.array([_[0] for _ in formation_energy_dissimilarities])
+    dissimilarities = get_dissimilarities(plugin1, plugin2, what=what)
+
+    flat_data = np.array([_[0] for _ in dissimilarities])
     
     if abs_x_range is None:
         half_range = np.sqrt(np.mean(np.array(flat_data)**2)) / x_zoom_factor
@@ -96,41 +132,50 @@ def generate_plots(plugin1, plugin2, x_zoom_factor=1., abs_x_range=None):
     pl.legend(loc='upper right')
     pl.xlabel("Formation energy dissimilarity (eV/atom)")
     pl.ylabel("Frequency")
-    pl.title(f"{plugin1} VS {plugin2}")
+    pl.title(f"{plugin1} VS {plugin2} ({what})")
     pl.xlim(-half_range, half_range)
     pl.tight_layout()
-    pl.savefig(f"{OUT_FOLDER}/histogram-{plugin1}-VS-{plugin2}.png")
+    pl.savefig(f"{OUT_FOLDER}/histogram-{what}-{plugin1}-VS-{plugin2}.png")
     pl.close(fig)
 
-    abs_formation_energy_dissimilarities = [
-        (abs(_[0]), _[1], _[2], _[3]) for _ in formation_energy_dissimilarities]
+    abs_dissimilarities = [
+        (abs(_[0]), _[1], _[2], _[3]) for _ in dissimilarities]
     
     # Sort in-place from the largest dissimilarity (in abs value)
-    abs_formation_energy_dissimilarities.sort()
-    abs_formation_energy_dissimilarities.reverse()
+    abs_dissimilarities.sort()
+    abs_dissimilarities.reverse()
 
-    with open(f"{OUT_FOLDER}/discrepancies-{plugin1}-VS-{plugin2}.txt", "w") as fhandle:
+    with open(f"{OUT_FOLDER}/discrepancies-{what}-{plugin1}-VS-{plugin2}.txt", "w") as fhandle:
         fhandle.write(f"## {plugin1} VS {plugin2}\n")
-        fhandle.write(f"## Cases with abs(Formation energies) > {FORM_ENERGY_PRINT_THRESHOLD} eV/atom:\n")
-        for form_energy_dissimilarity, system, form_energy_plugin1, form_energy_plugin2 in abs_formation_energy_dissimilarities:
-            if form_energy_dissimilarity < FORM_ENERGY_PRINT_THRESHOLD:
+        if what == 'formation-energy':
+            fhandle.write(f"## Cases with abs(formation energies) > {PRINT_THRESHOLD} eV/atom:\n")
+        elif what == 'unaries':
+            fhandle.write(f"## Cases with abs(energy difference) > {PRINT_THRESHOLD} eV/atom:\n")
+        else:
+            raise ValueError(f"Unknown value of 'what': '{what}'")
+        for dissimilarity, system, data_plugin1, data_plugin2 in abs_dissimilarities:
+            if dissimilarity < PRINT_THRESHOLD:
                 # Here I assume I already sorted them
                 break
-            fhandle.write(f"{system:30s}: {form_energy_dissimilarity:.6f} ({form_energy_plugin1} vs {form_energy_plugin2})\n")
+            fhandle.write(f"{system:30s}: {dissimilarity:.6f} ({data_plugin1} vs {data_plugin2})\n")
 
 if __name__ == "__main__":
     try:
-        plugin1 = sys.argv[1]
-        plugin2 = sys.argv[2]
-        zoom = sys.argv[3]
+        plugin1_name = sys.argv[1]
+        plugin2_name = sys.argv[2]
+        what = sys.argv[3]
+        zoom_value = sys.argv[4:5]
+        if zoom_value == []:
+            zoom_value = 1.
+        else:
+            zoom_value = zoom_value[0]
     except IndexError:
-        print("Pass as two parameters the two plugins to compare, and a third parameter with the zoom factor")
+        print("Pass as two parameters the two plugins to compare, a third parameter with 'what' to compare, and a fourth (optional) parameter as the zoom factor")
         sys.exit(1)
     try:
-        zoom = float(zoom)
+        zoom_value = float(zoom_value)
     except IndexError:
         print("The zoom factor must be a float number")
         sys.exit(1)
 
-    generate_plots(plugin1, plugin2, zoom)
-    print("*** IMPORTANT WARNING!!! THIS SCRIPT USES (STILL) THE EOS (ENERGIES AND VOLUMES) FROM THE SIMULATION CELL, AND NOT FROM PER FORMULA UNIT!! ***")
+    generate_plots(plugin1_name, plugin2_name, what, zoom_value)
