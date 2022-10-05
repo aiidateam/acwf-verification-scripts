5a7ce3242aa1359f37663dc42e26a2f53880ad92
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2022-10-05 09:44:22 +0200

    Adding option to only plot subset

--- extra_scripts/final_boxplot_all_codes/plot_box_all.py
+++ extra_scripts/final_boxplot_all_codes/plot_box_all.py
@@ -1,6 +1,7 @@
 # %%
 import json
 import pathlib as pl
+from more_itertools import only
 import numpy as np
 import matplotlib.pyplot as plt
 import tabulate
@@ -86,10 +87,20 @@ labels = [
         'abinit (PseudoDojo0.5b1)'
         ]
 
-def generate_box_plt(set_name, file_name):
+def generate_box_plt(set_name, file_name, only_must_have_elements=None, skip_codes=None):
+    if skip_codes is not None:
+        for skip_code in skip_codes:
+            try:
+                position = plugin_names.index(skip_code)
+                plugin_names.pop(position)
+                labels.pop(position)
+                print(f">>>>>> SKIPPING CODE '{skip_code}'")
+            except IndexError:
+                raise ValueError(f"Code '{skip_code}' asked to be skipped but does not exist!")
     all_data = {}
     for quantity_name in quantity_names:
         out_data = {}
+        print(f"#### {quantity_name} ####")
         for plugin_name in plugin_names:
             plugin_values = []
             plugin_big = 0
@@ -128,6 +139,29 @@ def generate_box_plt(set_name, file_name):
                 )
                 # Take the systems that are both in the reference and plugin sets
                 plot_systems = plugin_systems.intersection(ref_systems)
+                set_type = SET_NAME.partition('-')[0] # oxides or unaries
+                print(f"All systems: {len(plot_systems)} (set: {set_type}; full: {len(plugin_systems)}) for '{plugin_name}'")
+                if only_must_have_elements is not None:
+                    if set_type == 'oxides':
+                        variants = ['XO', 'XO2', 'X2O', 'X2O3', 'X2O5', 'XO3']
+                    elif set_type == 'unaries':
+                        variants = ['X/SC', 'X/FCC', 'X/BCC', 'X/Diamond']
+                    else:
+                        raise ValueError("Unrecognized set type!")
+                    missing = []
+                    new_plot_systems = set()
+                    for element in only_must_have_elements:
+                        for variant in variants:
+                            expected_key = f"{element}-{variant}"
+                            if expected_key not in plot_systems:
+                                missing.append(expected_key)
+                            else:
+                                new_plot_systems.add(expected_key)
+                    if missing:
+                        raise ValueError(f"{plugin_name} ({set_type}) misses the following keys: {missing}")
+                    plot_systems = new_plot_systems
+                print(f"   -> Plotting: {len(plot_systems)}")
+
 
                 for element_and_configuration in plot_systems:
                     element, configuration = element_and_configuration.split('-')
@@ -209,12 +243,15 @@ def generate_box_plt(set_name, file_name):
     fig.suptitle(f'Reference: all electron average.')
     fig.tight_layout()
 
-    fig.savefig(f'{file_name}.pdf')
+    suffix = "_SUBSET" if only_must_have_elements is not None else ""
+    fig.savefig(f'{file_name}{suffix}.pdf')
 
 
 if __name__ == "__main__":
+    import ase.data
     SET_NAME_1 = 'unaries-verification-PBE-v1'
     SET_NAME_2 = 'oxides-verification-PBE-v1'
+    ONLY_SUBSET = False
 
     try:
         mode = sys.argv[1]
@@ -222,11 +259,51 @@ if __name__ == "__main__":
         print("Pass as first parameter 'separate' (separate analysis for unaries and oxides) or 'all'.")
         sys.exit(1)
 
+    only_must_have_elements = None
+    skip_codes = None
+
+
+    if ONLY_SUBSET:
+        # ONLY PLOT A SUBSET, AND SKIP BIGDFT
+        
+        # except f
+        chemical_numbers = list(range(1, 56+1)) + list(range(72, 86+1))
+        # VASP
+        chemical_numbers.remove(1) #H
+        chemical_numbers.remove(2) #He
+        chemical_numbers.remove(4) #Be
+        chemical_numbers.remove(45) #Rh
+
+        # QE
+        chemical_numbers.remove(85) #At
+        chemical_numbers.remove(18) #Ar
+        chemical_numbers.remove(53) #I
+
+        # Siesta
+        chemical_numbers.remove(80) #Hg
+        chemical_numbers.remove(37) #Rb
+
+        # GPAW
+        chemical_numbers.remove(83) #Bi
+        chemical_numbers.remove(43) #Tc
+        chemical_numbers.remove(86) #Bi
+        chemical_numbers.remove(84) #Po
+
+        # CP2K
+        chemical_numbers.remove(11) #Na
+
+        only_must_have_elements = [ase.data.chemical_symbols[i] for i in
+            chemical_numbers]
+        skip_codes = ['bigdft']
+
     if mode == 'all':
-        generate_box_plt([SET_NAME_1, SET_NAME_2], 'box_plot_all')
+        generate_box_plt([SET_NAME_1, SET_NAME_2], 'box_plot_all',
+            only_must_have_elements=only_must_have_elements, skip_codes=skip_codes)
     elif mode == 'separate':
-        generate_box_plt([SET_NAME_1], 'box_plot_unaries')
-        generate_box_plt([SET_NAME_2], 'box_plt_oxides')
+        generate_box_plt([SET_NAME_1], 'box_plot_unaries',
+            only_must_have_elements=only_must_have_elements, skip_codes=skip_codes)
+        generate_box_plt([SET_NAME_2], 'box_plot_oxides',    
+            only_must_have_elements=only_must_have_elements, skip_codes=skip_codes)
     else:
         print("Pass as first parameter 'separate' (separate analysis for unaries and oxides) or 'all'.")
         sys.exit(1)
