5c5d1db2fe4468407ed4dc8d321d39ffd0b88a81
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2023-03-09 23:15:57 +0100

    Also comparing average values on the whole dataset

--- acwf_paper_plots/plots/supplementary/periodic_tables_all/compare-measures.py
+++ acwf_paper_plots/plots/supplementary/periodic_tables_all/compare-measures.py
@@ -1,19 +1,55 @@
 #!/usr/bin/env python
-import os
 import glob
 import json
 import copy
+
+import numpy as np
+
 from acwf_paper_plots.quantities_for_comparison import get_num_atoms_in_formula_unit
 
 PREFIX = "nu-unaries-"
 SUFFIX = "-vs-ae.json"
 ALL_MEASURES = ["nu", "epsilon", "delta_per_formula_unit"]
 
+# List of unaries that are from the Science 2016 paper that also in our set
+overlapping_elements = [
+    "Ag-X/FCC",
+    "Al-X/FCC",
+    "Ar-X/FCC",
+    "Au-X/FCC",
+    "Ca-X/FCC",
+    "Cu-X/FCC",
+    "Ir-X/FCC",
+    "Kr-X/FCC",
+    "Ne-X/FCC",
+    "Pb-X/FCC",
+    "Pd-X/FCC",
+    "Pt-X/FCC",
+    "Rh-X/FCC",
+    "Rn-X/FCC",
+    "Sr-X/FCC",
+    "Xe-X/FCC",
+    "Ba-X/BCC",
+    "Cs-X/BCC",
+    "K-X/BCC",
+    "Mo-X/BCC",
+    "Nb-X/BCC",
+    "Rb-X/BCC",
+    "Ta-X/BCC",
+    "V-X/BCC",
+    "W-X/BCC",
+    "Po-X/SC",
+    "Ge-X/Diamond",
+    "Si-X/Diamond",
+    "Sn-X/Diamond"
+]
+
 all_methods = []
 # assume that if the file for nu and unaries is there, all other files are there as well
 for filename in glob.glob(f"{PREFIX}*{SUFFIX}"):
     method_short_name = filename[len(PREFIX):-len(SUFFIX)]
     all_methods.append(method_short_name)
+all_methods = sorted(all_methods)
 
 data = {}
 for measure in ALL_MEASURES:
@@ -53,8 +89,8 @@ for method in all_methods:
             data_for_measures[measure].append(data[measure][method][key])
     flat_data[method] = copy.deepcopy(data_for_measures)
 
+## Generate pairwise comparison of metrics
 import pylab as pl
-
 for (meas1, meas1name), (meas2, meas2name), file_basename in [
     [
         ("epsilon", r"$\varepsilon$"),
@@ -85,4 +121,36 @@ for (meas1, meas1name), (meas2, meas2name), file_basename in [
     pl.savefig(filename)
     print(f"File '{filename}' written.")
 
-    
+## Compare delta on old set with nu and epsilon on new set
+print("# METHOD EPS_AVERAGE NU_AVERAGE DELTA_AVERAGE_SCIENCE_SUBSET")
+all_eps_average = []
+all_nu_average = []
+all_delta_subset_average = []
+for method in all_methods:
+    eps_data = list(data['epsilon'][method].values())
+    nu_data = list(data['nu'][method].values())
+    delta_subset_data = [v for k, v in data['delta_per_formula_unit'][method].items() if k in overlapping_elements]
+    #print(f"# Method: {method} ({len(eps_data)}/960 systems, {len(delta_subset_data)}/{len(overlapping_elements)} in the Delta subset)")
+    #print(f"#  - average epsilon          : {np.mean(eps_data)}")
+    #print(f"#  - average nu               : {np.mean(nu_data)}")
+    #print(f"#  - average delta (on subset): {np.mean(delta_subset_data)}")
+    print(f"{method} {np.mean(eps_data)} {np.mean(nu_data)} {np.mean(delta_subset_data)}")
+    all_eps_average.append(np.mean(eps_data))
+    all_nu_average.append(np.mean(nu_data))
+    all_delta_subset_average.append(np.mean(delta_subset_data))
+
+for xdata, xlabel, ydata, ylabel, filename in [
+    [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_eps_average, r"Average $\varepsilon$", "average-delta-vs-eps-on-science-subset.png"],
+    [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_nu_average, r"Average $\nu$", "average-delta-vs-nu-on-science-subset.png"],
+    [all_eps_average, r"Average $\varepsilon$", all_nu_average, r"Average $\nu$", "average-eps-vs-nu-on-science-subset.png"],
+]:
+    pl.figure()
+    print(xdata)
+    print(ydata)
+    pl.plot(xdata, ydata, 'o')
+    for label, x, y in zip(all_methods, xdata, ydata):
+        pl.annotate(label, (x, y))
+    pl.xlabel(xlabel)
+    pl.ylabel(ylabel)
+    pl.savefig(filename)
+    print(f"File '{filename}' written.")
