4c84c28723c76a0d80833b3307d1b4c5d14ec1f7
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2023-03-16 18:06:21 +0100

    Running both LOGLOG True and False, and fixing an error in the value of the theoretical fit between epsilon and nu

--- acwf_paper_plots/plots/supplementary/periodic_tables_all/compare-measures.py
+++ acwf_paper_plots/plots/supplementary/periodic_tables_all/compare-measures.py
@@ -10,7 +10,6 @@ from acwf_paper_plots.quantities_for_comparison import get_num_atoms_in_formula_
 PREFIX = "nu-unaries-"
 SUFFIX = "-vs-ae.json"
 ALL_MEASURES = ["nu", "epsilon", "delta_per_formula_unit", "delta_per_formula_unit_over_b0"]
-LOGLOG = True
 
 # List of unaries that are from the Science 2016 paper that also in our set
 overlapping_elements = [
@@ -92,177 +91,178 @@ for method in all_methods:
 
 ## Generate pairwise comparison of metrics
 import pylab as pl
-# factor1 and 2 are used to change units
-for (meas1, meas1name, factor1), (meas2, meas2name, factor2), file_basename in [
-    [
-        ("epsilon", r"$\varepsilon$", 1.),
-        ("nu", r"$\nu$", 1.),
-        "epsilon-vs-nu"
-    ],
-    [
-        ("delta_per_formula_unit", r"$\Delta$ per atom (meV/atom)", 1.),
-        ("epsilon", r"$\varepsilon$", 1.),
-        # Note: above I have converted it in delta per atom
-        "delta-vs-epsilon"
-    ],
-    [
-        # Delta is in meV. B_0 is in eV/ang^3.
-        # If I want to get Ang^-3, I need to divide by 1000
-        ("delta_per_formula_unit_over_b0", r"$\Delta/B_0$ per atom ($\AA^{-3}$)", 0.001),
-        ("epsilon", r"$\varepsilon$", 1.),
-        # Note: above I have converted it in delta per atom
-        "deltaoverB0-vs-epsilon"
-    ],    
-    [
-        # Note: above I have converted it in delta per atom
-        ("delta_per_formula_unit", r"$\Delta$ per atom (meV/atom)", 1.),
-        ("nu", r"$\nu$", 1.),
-        "delta-vs-nu"
-    ],        
-]:
-    
-    if not LOGLOG and meas1 == 'epsilon' and meas2 == 'nu':
-        fig = pl.figure(figsize=(8, 4))
-        pl.subplot(121)
-    else:
-        fig = pl.figure(figsize=(4, 3))
-        fig = pl.figure()
-    all_data_x = []
-    all_data_y = []
-    for method in all_methods:
-        all_data_x += (np.array(flat_data[method][meas1]) * factor1).tolist()
-        all_data_y += (np.array(flat_data[method][meas2]) * factor2).tolist()
+for LOGLOG in [True, False]:
+    # factor1 and 2 are used to change units
+    for (meas1, meas1name, factor1), (meas2, meas2name, factor2), file_basename in [
+        [
+            ("epsilon", r"$\varepsilon$", 1.),
+            ("nu", r"$\nu$", 1.),
+            "epsilon-vs-nu"
+        ],
+        [
+            ("delta_per_formula_unit", r"$\Delta$ per atom (meV/atom)", 1.),
+            ("epsilon", r"$\varepsilon$", 1.),
+            # Note: above I have converted it in delta per atom
+            "delta-vs-epsilon"
+        ],
+        [
+            # Delta is in meV. B_0 is in eV/ang^3.
+            # If I want to get Ang^-3, I need to divide by 1000
+            ("delta_per_formula_unit_over_b0", r"$\Delta/B_0$ per atom ($\AA^{3}$)", 0.001),
+            ("epsilon", r"$\varepsilon$", 1.),
+            # Note: above I have converted it in delta per atom
+            "deltaoverB0-vs-epsilon"
+        ],    
+        [
+            # Note: above I have converted it in delta per atom
+            ("delta_per_formula_unit", r"$\Delta$ per atom (meV/atom)", 1.),
+            ("nu", r"$\nu$", 1.),
+            "delta-vs-nu"
+        ],        
+    ]:
+        
+        if not LOGLOG and meas1 == 'epsilon' and meas2 == 'nu':
+            fig = pl.figure(figsize=(8, 4))
+            pl.subplot(121)
+        else:
+            fig = pl.figure(figsize=(4, 3))
+            fig = pl.figure()
+        all_data_x = []
+        all_data_y = []
+        for method in all_methods:
+            all_data_x += (np.array(flat_data[method][meas1]) * factor1).tolist()
+            all_data_y += (np.array(flat_data[method][meas2]) * factor2).tolist()
+            if LOGLOG:
+                pl.loglog(np.array(flat_data[method][meas1]) * factor1, np.array(flat_data[method][meas2]) * factor2, '.', color='#2b8cbe', label=method)
+            else:
+                pl.plot(np.array(flat_data[method][meas1]) * factor1, np.array(flat_data[method][meas2]) * factor2, '.', color='#2b8cbe', label=method)
         if LOGLOG:
-            pl.loglog(np.array(flat_data[method][meas1]) * factor1, np.array(flat_data[method][meas2]) * factor2, '.', color='#2b8cbe', label=method)
-        else:
-            pl.plot(np.array(flat_data[method][meas1]) * factor1, np.array(flat_data[method][meas2]) * factor2, '.', color='#2b8cbe', label=method)
-    if LOGLOG:
-        # Cache limits
-        xlim = pl.xlim()
-        ylim = pl.ylim()
+            # Cache limits
+            xlim = pl.xlim()
+            ylim = pl.ylim()
 
-        if meas1 == 'epsilon' and meas2 == 'nu':
-            xmin = 1e-3; xmax = 1; slope = 1.673
+            if meas1 == 'epsilon' and meas2 == 'nu':
+                xmin = 1e-3; xmax = 1; slope = 1.673
 
-            for m1_threshold, m2_threshold, color in [
-                (0.06, 0.1, 'green'),
-                (0.2, 0.35, 'red'),
-            ]:
-                pl.plot([xlim[0], m2_threshold / slope], [m2_threshold, m2_threshold], '--', color=color, linewidth=0.5)
-                pl.plot([m1_threshold, m1_threshold], [ylim[0], m1_threshold * slope], '--', color=color, linewidth=0.5)
-                pl.annotate('(d)', (0.03, 0.95), xycoords='axes fraction')
+                for m1_threshold, m2_threshold, color in [
+                    (0.06, 0.1, 'green'),
+                    (0.2, 0.35, 'red'),
+                ]:
+                    pl.plot([xlim[0], m2_threshold / slope], [m2_threshold, m2_threshold], '--', color=color, linewidth=0.5)
+                    pl.plot([m1_threshold, m1_threshold], [ylim[0], m1_threshold * slope], '--', color=color, linewidth=0.5)
+                    pl.annotate('(d)', (0.03, 0.95), xycoords='axes fraction')
 
-        elif meas1 == 'delta_per_formula_unit' and meas2 == 'epsilon':
-            xmin = 1e-3; xmax = 10; slope = 0.2
+            elif meas1 == 'delta_per_formula_unit' and meas2 == 'epsilon':
+                xmin = 1e-3; xmax = 10; slope = 0.2
 
-            for m1_threshold, m2_threshold, color in [
-                (0.3, 0.06, 'green'),
-                (0.95, 0.2, 'red'),
-            ]:
-                pl.plot([xlim[0], m2_threshold / slope], [m2_threshold, m2_threshold], '--', color=color, linewidth=0.5)
-                pl.plot([m1_threshold, m1_threshold], [ylim[0], m1_threshold * slope], '--', color=color, linewidth=0.5)
-                pl.annotate('(a)', (0.03, 0.95), xycoords='axes fraction')
+                for m1_threshold, m2_threshold, color in [
+                    (0.3, 0.06, 'green'),
+                    (0.95, 0.2, 'red'),
+                ]:
+                    pl.plot([xlim[0], m2_threshold / slope], [m2_threshold, m2_threshold], '--', color=color, linewidth=0.5)
+                    pl.plot([m1_threshold, m1_threshold], [ylim[0], m1_threshold * slope], '--', color=color, linewidth=0.5)
+                    pl.annotate('(a)', (0.03, 0.95), xycoords='axes fraction')
 
-        elif meas1 == 'delta_per_formula_unit' and meas2 == 'nu':
-            xmin = 2e-3; xmax = 30; slope = 0.33
+            elif meas1 == 'delta_per_formula_unit' and meas2 == 'nu':
+                xmin = 2e-3; xmax = 30; slope = 0.33
 
-            for m1_threshold, m2_threshold, color in [
-                (0.3, 0.1, 'green'),
-                (0.95, 0.35, 'red'),
-            ]:
-                pl.plot([xlim[0], m2_threshold / slope], [m2_threshold, m2_threshold], '--', color=color, linewidth=0.5)
-                pl.plot([m1_threshold, m1_threshold], [ylim[0], m1_threshold * slope], '--', color=color, linewidth=0.5)
-                pl.annotate('(b)', (0.03, 0.95), xycoords='axes fraction')
+                for m1_threshold, m2_threshold, color in [
+                    (0.3, 0.1, 'green'),
+                    (0.95, 0.35, 'red'),
+                ]:
+                    pl.plot([xlim[0], m2_threshold / slope], [m2_threshold, m2_threshold], '--', color=color, linewidth=0.5)
+                    pl.plot([m1_threshold, m1_threshold], [ylim[0], m1_threshold * slope], '--', color=color, linewidth=0.5)
+                    pl.annotate('(b)', (0.03, 0.95), xycoords='axes fraction')
 
-        elif meas1 == 'delta_per_formula_unit_over_b0' and meas2 == 'epsilon':
-            xmin = 1e-5; xmax = 1e-2; slope = 100.
-            pl.annotate('(c)', (0.03, 0.95), xycoords='axes fraction')
-        else:
-            xmin = None
-        if xmin is not None:
-            # Plot line of slope = 1 (i.e. linear relationship)
-            pl.loglog([xmin, xmax], [xmin * slope, xmax * slope], '-k')
-        # Put back cached limits
-        pl.xlim(xlim)
-        pl.ylim(ylim)
+            elif meas1 == 'delta_per_formula_unit_over_b0' and meas2 == 'epsilon':
+                xmin = 1e-5; xmax = 1e-2; slope = 100.
+                pl.annotate('(c)', (0.03, 0.95), xycoords='axes fraction')
+            else:
+                xmin = None
+            if xmin is not None:
+                # Plot line of slope = 1 (i.e. linear relationship)
+                pl.loglog([xmin, xmax], [xmin * slope, xmax * slope], '-k')
+            # Put back cached limits
+            pl.xlim(xlim)
+            pl.ylim(ylim)
 
-    pl.xlabel(meas1name)
-    pl.ylabel(meas2name)
-    #pl.legend(loc='best')
-    if not LOGLOG and meas1 == 'epsilon' and meas2 == 'nu':
-        all_data_x = np.array(all_data_x)
-        all_data_y = np.array(all_data_y)
-        threshold = 1.
-        filter = np.copy(all_data_x < threshold)
-        all_data_x = all_data_x[filter]
-        all_data_y = all_data_y[filter]
-        pl.subplot(122)
-        pl.plot(all_data_x, all_data_y, '.', color='#2b8cbe')
         pl.xlabel(meas1name)
         pl.ylabel(meas2name)
+        #pl.legend(loc='best')
+        if not LOGLOG and meas1 == 'epsilon' and meas2 == 'nu':
+            all_data_x = np.array(all_data_x)
+            all_data_y = np.array(all_data_y)
+            threshold = 1.
+            filter = np.copy(all_data_x < threshold)
+            all_data_x = all_data_x[filter]
+            all_data_y = all_data_y[filter]
+            pl.subplot(122)
+            pl.plot(all_data_x, all_data_y, '.', color='#2b8cbe')
+            pl.xlabel(meas1name)
+            pl.ylabel(meas2name)
 
 
-        # Linear fit, see https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html
-        A = np.vstack([all_data_x, np.ones(len(all_data_x))]).T
-        m, c = np.linalg.lstsq(A, all_data_y, rcond=None)[0]
-        print(f">> eps-vs-nu fit (on linear scale): nu = {m} * eps + {c}")
-        xmin, xmax = all_data_x.min(), all_data_x.max()
-        pl.plot([xmin, xmax], [m*xmin + c, m*xmax + c], 'r', label='Linear fit') #label=f'Fit (m={m:.3f}, c={c:.3f})')
-        m_theor = 2 * np.sqrt(7) / np.sqrt(10) #  ~1.67332
-        pl.plot([xmin, xmax], [m_theor*xmin, m_theor*xmax], 'y', label='Theoretical relation')#label=f'Theoretical (m={m_theor:.3f}, c=0)')
-        pl.xlim(0, threshold)
-        pl.ylim(0, threshold * m_theor)
-        pl.legend(loc='lower right')
+            # Linear fit, see https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html
+            A = np.vstack([all_data_x, np.ones(len(all_data_x))]).T
+            m, c = np.linalg.lstsq(A, all_data_y, rcond=None)[0]
+            print(f">> eps-vs-nu fit (on linear scale): nu = {m} * eps + {c}")
+            xmin, xmax = all_data_x.min(), all_data_x.max()
+            pl.plot([xmin, xmax], [m*xmin + c, m*xmax + c], 'r', label='Linear fit') #label=f'Fit (m={m:.3f}, c={c:.3f})')
+            m_theor =6. / np.sqrt(15) #  ~1.549
+            pl.plot([xmin, xmax], [m_theor*xmin, m_theor*xmax], 'y', label='Theoretical relation')#label=f'Theoretical (m={m_theor:.3f}, c=0)')
+            pl.xlim(0, threshold)
+            pl.ylim(0, threshold * m_theor)
+            pl.legend(loc='lower right')
 
 
-    if not LOGLOG and meas1 == "delta_per_formula_unit_over_b0":
-        xlim_max = 750
+        if not LOGLOG and meas1 == "delta_per_formula_unit_over_b0":
+            xlim_max = 750
 
-        outliers_count = 0
-        for method in all_methods:
-            outliers_count += sum(np.array(flat_data[method][meas1]) > xlim_max)
+            outliers_count = 0
+            for method in all_methods:
+                outliers_count += sum(np.array(flat_data[method][meas1]) > xlim_max)
 
-        pl.xlim(0, xlim_max)
-        pl.title(f"{outliers_count} outliers on the right")
+            pl.xlim(0, xlim_max)
+            pl.title(f"{outliers_count} outliers on the right")
 
-    filename = f"comparison-{file_basename}{'-loglog' if LOGLOG else ''}.png"
-    pl.savefig(filename)
-    print(f"File '{filename}' written.")
+        filename = f"comparison-{file_basename}{'-loglog' if LOGLOG else ''}.png"
+        pl.savefig(filename)
+        print(f"File '{filename}' written.")
 
-## Compare delta on old set with nu and epsilon on new set
-print("# METHOD EPS_AVERAGE NU_AVERAGE DELTA_AVERAGE_SCIENCE_SUBSET")
-all_eps_average = []
-all_nu_average = []
-all_delta_average = []
-all_delta_subset_average = []
-for method in all_methods:
-    eps_data = list(data['epsilon'][method].values())
-    nu_data = list(data['nu'][method].values())
-    delta_data = list(data['delta_per_formula_unit'][method].values())
-    delta_subset_data = [v for k, v in data['delta_per_formula_unit'][method].items() if k in overlapping_elements]
-    #print(f"# Method: {method} ({len(eps_data)}/960 systems, {len(delta_subset_data)}/{len(overlapping_elements)} in the Delta subset)")
-    #print(f"#  - average epsilon          : {np.mean(eps_data)}")
-    #print(f"#  - average nu               : {np.mean(nu_data)}")
-    #print(f"#  - average delta (on subset): {np.mean(delta_subset_data)}")
-    print(f"{method} {np.mean(eps_data)} {np.mean(nu_data)} {np.mean(delta_subset_data)}")
-    all_eps_average.append(np.mean(eps_data))
-    all_nu_average.append(np.mean(nu_data))
-    all_delta_average.append(np.mean(delta_data))
-    all_delta_subset_average.append(np.mean(delta_subset_data))
+    ## Compare delta on old set with nu and epsilon on new set
+    print("# METHOD EPS_AVERAGE NU_AVERAGE DELTA_AVERAGE_SCIENCE_SUBSET")
+    all_eps_average = []
+    all_nu_average = []
+    all_delta_average = []
+    all_delta_subset_average = []
+    for method in all_methods:
+        eps_data = list(data['epsilon'][method].values())
+        nu_data = list(data['nu'][method].values())
+        delta_data = list(data['delta_per_formula_unit'][method].values())
+        delta_subset_data = [v for k, v in data['delta_per_formula_unit'][method].items() if k in overlapping_elements]
+        #print(f"# Method: {method} ({len(eps_data)}/960 systems, {len(delta_subset_data)}/{len(overlapping_elements)} in the Delta subset)")
+        #print(f"#  - average epsilon          : {np.mean(eps_data)}")
+        #print(f"#  - average nu               : {np.mean(nu_data)}")
+        #print(f"#  - average delta (on subset): {np.mean(delta_subset_data)}")
+        print(f"{method} {np.mean(eps_data)} {np.mean(nu_data)} {np.mean(delta_subset_data)}")
+        all_eps_average.append(np.mean(eps_data))
+        all_nu_average.append(np.mean(nu_data))
+        all_delta_average.append(np.mean(delta_data))
+        all_delta_subset_average.append(np.mean(delta_subset_data))
 
-for xdata, xlabel, ydata, ylabel, filename in [
-    [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_eps_average, r"Average $\varepsilon$", "average-delta-vs-eps-on-science-subset.png"],
-    [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_nu_average, r"Average $\nu$", "average-delta-vs-nu-on-science-subset.png"],
-    [all_eps_average, r"Average $\varepsilon$", all_nu_average, r"Average $\nu$", "average-eps-vs-nu-on-science-subset.png"],
-    [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_delta_average, r"Average $\Delta$ per atom (full set)", "average-delta-subset-vs-full-delta-on-science-subset.png"],
-]:
-    pl.figure()
-    #print(xdata)
-    #print(ydata)
-    pl.plot(xdata, ydata, 'o')
-    for label, x, y in zip(all_methods, xdata, ydata):
-        pl.annotate(label, (x, y))
-    pl.xlabel(xlabel)
-    pl.ylabel(ylabel)
-    pl.savefig(filename)
-    print(f"File '{filename}' written.")
+    for xdata, xlabel, ydata, ylabel, filename in [
+        [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_eps_average, r"Average $\varepsilon$", "average-delta-vs-eps-on-science-subset.png"],
+        [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_nu_average, r"Average $\nu$", "average-delta-vs-nu-on-science-subset.png"],
+        [all_eps_average, r"Average $\varepsilon$", all_nu_average, r"Average $\nu$", "average-eps-vs-nu-on-science-subset.png"],
+        [all_delta_subset_average, r"Average $\Delta$ per atom (on Science 2016 subset)", all_delta_average, r"Average $\Delta$ per atom (full set)", "average-delta-subset-vs-full-delta-on-science-subset.png"],
+    ]:
+        pl.figure()
+        #print(xdata)
+        #print(ydata)
+        pl.plot(xdata, ydata, 'o')
+        for label, x, y in zip(all_methods, xdata, ydata):
+            pl.annotate(label, (x, y))
+        pl.xlabel(xlabel)
+        pl.ylabel(ylabel)
+        pl.savefig(filename)
+        print(f"File '{filename}' written.")
