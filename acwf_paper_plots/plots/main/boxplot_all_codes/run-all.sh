77bafe81ef4dcedec2d12750fe30bb0bb307a740
Jusong Yu <jusong.yeu@gmail.com>
2023-03-24 11:04:35 +0100

    Actinides results added to SSSP 1.3

--- acwf_paper_plots/plots/main/boxplot_all_codes/run-all.sh
+++ acwf_paper_plots/plots/main/boxplot_all_codes/run-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./plot_box_all.py together up-to-Bi-no-lanthanides
 ./plot_box_all.py together only-lanthanides "WIEN2k@(L)APW+lo+LO" "FLEUR@LAPW+LO" "CASTEP@PW|C19MK2" "VASP@PW|GW-PAW54*" "Quantum ESPRESSO@PW|SSSP-prec-v1.2"
-./plot_box_all.py together only-actinides "WIEN2k@(L)APW+lo+LO" "FLEUR@LAPW+LO" "CASTEP@PW|C19MK2" "VASP@PW|GW-PAW54*"
+./plot_box_all.py together only-actinides "WIEN2k@(L)APW+lo+LO" "FLEUR@LAPW+LO" "CASTEP@PW|C19MK2" "VASP@PW|GW-PAW54*" "Quantum ESPRESSO@PW|SSSP-prec-v1.2"
